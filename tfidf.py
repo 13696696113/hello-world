fa775337327df1b2b686693c461e1bd16af5640a
13696696113 <44639954+13696696113@users.noreply.github.com>
2022-08-15 18:04:35 +0800

    Update and rename tfidf to tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -1,3 +1,5 @@
+# https://www.jianshu.com/p/14b26f59040b
+
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer  
 from sklearn.feature_extraction.text import CountVectorizer  
